a4b653e6d730a1fb74bee1d6555131637c573212
UtopiOne <arseniyver@gmail.com>
2023-03-26 20:48:47 +0300

    Added changing numbers

--- Enemies/scenes/enemy.gd
+++ Enemies/scenes/enemy.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+signal hit(number)
+
 @export var corresponding_number = 0
 const move_speed = 900
 var velocity = Vector2(-1 * move_speed, 0)
@@ -10,4 +12,6 @@ func _ready():
 func _integrate_forces(state):
 	linear_velocity = velocity
 	
+func _on_body_entered(body):
+	hit.emit(corresponding_number)
 
--- Player/Projectile/Projectile.gd
+++ Player/Projectile/Projectile.gd
@@ -5,10 +5,11 @@ var speed = 20
 func _physics_process(delta):
 	position.x += speed 
 	
-func _on_Projectile_area_entered(area):
+func _on_Projectile_areaasd_entered(area):
 	queue_free()
 
 
 func _on_Projectile_body_entered(body):
+	Enemy.corresponding_number = body.corresponding_number
 	body.queue_free()
 	queue_free()
